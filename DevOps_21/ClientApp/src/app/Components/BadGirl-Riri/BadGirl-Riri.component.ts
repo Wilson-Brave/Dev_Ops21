e72b0c545dc664f34b7e780db24982c6e4c9d310
WillyWonker69 <58514857+WillyWonker69@users.noreply.github.com>
2021-08-12 00:19:36 +0200

    initial commit

---
+++ DevOps_21/ClientApp/src/app/Components/BadGirl-Riri/BadGirl-Riri.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-BadGirl-Riri',
+  templateUrl: './BadGirl-Riri.component.html',
+  styleUrls: ['./BadGirl-Riri.component.css']
+})
+export class BadGirlRiriComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ DevOps_21/ClientApp/src/app/Universe-Exploration/Universe-Exploration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Universe-Exploration',
+  templateUrl: './Universe-Exploration.component.html',
+  styleUrls: ['./Universe-Exploration.component.css']
+})
+export class UniverseExplorationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
